f2257cd8ec5b235a1ec2804023c948c0f4fb7940
pmltechpile <126093677+pmltechpile@users.noreply.github.com>
2023-12-06 16:03:13 -0600

    fixing comment placement

--- src/modules/products/components/product-tabs/index.tsx
+++ src/modules/products/components/product-tabs/index.tsx
@@ -72,13 +72,15 @@ const ProductInfoTab = ({ product }: ProductTabsProps) => {
           </div>
           <div>
             <span className="font-semibold">Courses</span>
-            {/* @ts-ignore */}
-            <p>{product.metadata?.courses ? product.metadata?.courses : "-"}</p>
+            <p>
+              {/* @ts-ignore */}
+              {product.metadata?.courses ? product.metadata?.courses : "-"}
+            </p>
           </div>
           <div>
             <span className="font-semibold">Class Duration</span>
-            {/* @ts-ignore */}
             <p>
+              {/* @ts-ignore */}
               {product.metadata?.duration ? product.metadata?.duration : "-"}
             </p>
           </div>
