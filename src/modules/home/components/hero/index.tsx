4261b768c5ba38622de8566e8a3a43ac144348af
pmltechpile <126093677+pmltechpile@users.noreply.github.com>
2023-12-08 22:54:54 -0600

    text color changes

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <div className="h-[100vh] w-full relative">
       <div className="text-black absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-start small:items-start small:p-32">
-        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black w-full sm:w-1/2 small:w-1/3 text-white">
+        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black w-full sm:w-1/2 small:w-1/3 sm:text-white lg:text-black">
           Unleash Your Culinary Creativity with Chef Velez&apos;s Cooking Events
         </h1>
-        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black text-white">
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black sm:text-white lg:text-black">
           Embark on a culinary journey like never before with our curated
           cooking events. Dive into a world of flavors and techniques that will
           elevate your skills to new heights. Get ready to savor the art of
